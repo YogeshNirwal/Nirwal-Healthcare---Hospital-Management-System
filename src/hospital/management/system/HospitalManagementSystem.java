8a4c22572a4aba71788aba33e5cb09a373fb95b4
YogeshNirwal <nirwalyogesh92@gmail.com>
2024-09-24 16:29:24 +0530

    first commit

---
+++ src/hospital/management/system/HospitalManagementSystem.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hospital.management.system;
+
+/**
+ *
+ * @author Yogesh Nirwal
+ */
+public class HospitalManagementSystem {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frontPage flogin=  new frontPage();
+              flogin.setVisible(true);
+              Login.closeFlogin(flogin);
+    }
+    
+}
